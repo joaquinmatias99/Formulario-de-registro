29c1b224ec001b700d7cc9d794530fe83f308642
joaquinmatias99 <joaquinmatias99@gmail.com>
2024-05-30 19:59:59 -0300

    funcion base

--- script.js
+++ script.js
@@ -2,7 +2,36 @@
     document.body.classList.toggle('tema2');
 });
 */
-function cambiarTema()
-{
+
+let nombre = document.getElementById('inputNombre');
+let email = document.getElementById('inputEmail');
+let contraseña = document.getElementById('inputContraseña');
+let repetirContraseña = document.getElementById('inputRepetirContraseña');
+
+document.getElementById('botonRegistrar').addEventListener('click', registrar());
+
+
+function registrar() {
+    if (!nombre) {
+        if(!email)
+            {
+                if(contraseña==repetirContraseña)
+                    {
+                        console.log("Registrado");
+                    }
+            }
+            else{
+                
+            }
+
+    }
+    else {
+
+    }
+}
+
+
+
+function cambiarTema() {
     document.body.classList.toggle('tema2');
 }
